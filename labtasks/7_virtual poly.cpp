b42aa576033b825f7e0a028cbbc739f1076fa3e3
Ritovash Chanda <ritovash5091@gmail.com>
2026-01-08 00:29:39 +0600

    Update 7_virtual poly.cpp

--- labtasks/7_virtual poly.cpp
+++ labtasks/7_virtual poly.cpp
@@ -1,27 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Bill{
+public:
     int num;
-    public:
+    Bill(int a):num(a){};
     virtual float revenue()=0;
 };
 class General:public Bill{
     public:
-    void revenue(int n){
-        return 500*n;
-    }
+    General(int a):Bill(a){} 
+    float revenue(){return 500*num;}
 };
 class Surgery:public Bill{
     public:
-    void revenue(int n){
-        return 2000*n;
-    }
+    Surgery(int a):Bill(a){}
+    float revenue(){return 2000*num;}
 };
 int main(){
     int n;
     cin>>n;
+    string type;
+    int num;
+    float revenue=0;
     while(n--){
-        cin>>type>>patient;
+        cin>>type>>num;
+        Bill *a = nullptr;
+        if(type=="General" || type=="general"){
+            a = new General(num);
+        }
+        else if(type=="Surgery" || type=="surgery"){
+            a = new Surgery(num);
+        }
+        else{
+            cout<<"Invalid"<<endl;
+        }
+        revenue+=a->revenue();
+        delete a;
     }
-
+    cout<<revenue<<endl;
 }
